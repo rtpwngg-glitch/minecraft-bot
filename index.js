2d9ba66ebf4b0a88839a288cacf20e82135f91be
rtpwngg-glitch <rr7744011@gmail.com>
2026-07-09 20:47:35 +0700

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
+const mineflayer = require('mineflayer')
 
+const bot = mineflayer.createBot({
+  host: 'furina.nexuscloud.id',
+  port: 19092,
+  username: 'NamaBot', // Ganti dengan username bot yang kamu inginkan
+  auth: 'offline'
+})
+
+bot.on('login', () => {
+  console.log('Bot berhasil login!')
+})
+
+bot.on('spawn', () => {
+  console.log('Bot sudah masuk ke server!')
+})
+
+bot.on('kicked', (reason) => {
+  console.log('Bot dikeluarkan:', reason)
+})
+
+bot.on('error', (err) => {
+  console.log('Error:', err)
+})
+
+bot.on('end', () => {
+  console.log('Bot terputus, mencoba reconnect dalam 5 detik...')
+  setTimeout(() => {
+    require('./index.js')
+  }, 5000)
+})
